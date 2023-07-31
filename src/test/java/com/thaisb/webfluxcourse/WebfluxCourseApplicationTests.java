d0813bf8311e3d0ff2a4a496600937dfb221ef25
thaisBertoldi <thais_brt@hotmail.com>
2023-07-31 19:08:14 -0300

    init: projeto iniciado

---
+++ src/test/java/com/thaisb/webfluxcourse/WebfluxCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thaisb.webfluxcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
