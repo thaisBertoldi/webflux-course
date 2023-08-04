76f46a2a133d036a6c59c23dee139b3dc5c509db
thaisBertoldi <thais_brt@hotmail.com>
2023-08-04 19:14:38 -0300

    feat: inclusao de interface controller do User

---
+++ src/main/java/com/thaisb/webfluxcourse/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.thaisb.webfluxcourse.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface UserController {
+
+    @PostMapping
+    ResponseEntity<Mono<Void>> save(@RequestBody UserRequest request);
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Mono<UserResponse>> findById(@PathVariable String id);
+
+    @GetMapping
+    ResponseEntity<Flux<UserResponse>> findAll();
+
+    @PatchMapping(value = "/{id}")
+    ResponseEntity<Mono<UserResponse>> update(@PathVariable String id, @RequestBody UserRequest userRequest);
+
+    @DeleteMapping(value = "/{id}")
+    ResponseEntity<Mono<Void>> deleteById(@PathVariable String id);
+
+}
