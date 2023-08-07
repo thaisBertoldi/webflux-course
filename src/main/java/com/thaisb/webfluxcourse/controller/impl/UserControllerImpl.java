aad213e4fecc5025f030a99be878367c31241d0a
thaisBertoldi <thais_brt@hotmail.com>
2023-08-07 20:11:23 -0300

    feat: implementacao do metodo save

--- src/main/java/com/thaisb/webfluxcourse/controller/impl/UserControllerImpl.java
+++ src/main/java/com/thaisb/webfluxcourse/controller/impl/UserControllerImpl.java
@@ -3,6 +3,9 @@ package com.thaisb.webfluxcourse.controller.impl;
 import com.thaisb.webfluxcourse.controller.UserController;
 import com.thaisb.webfluxcourse.model.request.UserRequest;
 import com.thaisb.webfluxcourse.model.response.UserResponse;
+import com.thaisb.webfluxcourse.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +13,16 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(value = "/users")
 public class UserControllerImpl implements UserController {
+
+    private final UserService service;
+
     @Override
     public ResponseEntity<Mono<Void>> save(UserRequest request) {
-        return null;
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(service.save(request).then());
     }
 
     @Override
