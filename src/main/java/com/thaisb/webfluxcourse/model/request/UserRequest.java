cdc0ca488b7056ae86533265d1b9841c8407de90
thaisBertoldi <thais_brt@hotmail.com>
2023-08-12 18:27:43 -0300

    feat: incluindo a validacao no model

--- src/main/java/com/thaisb/webfluxcourse/model/request/UserRequest.java
+++ src/main/java/com/thaisb/webfluxcourse/model/request/UserRequest.java
@@ -1,17 +1,23 @@
 package com.thaisb.webfluxcourse.model.request;
 
+import com.thaisb.webfluxcourse.validator.TrimStringValidation;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequest(
 
+    @TrimStringValidation
     @Size(min = 3, max = 50, message = "O nome precisa ter entre 3 e 50 caracteres.")
     @NotBlank(message = "Nome não pode ser nulo ou vazio.")
     String name,
+
+    @TrimStringValidation
     @Email(message = "Email inválido.")
     @NotBlank(message = "Email nao pode ser nulo ou vazio.")
     String email,
+
+    @TrimStringValidation
     @Size(min = 3, max = 20, message = "A senha precisa ter entre 3 e 20 caracteres.")
     @NotBlank(message = "Senha não pode ser nulo ou vazio.")
     String password
